d54470981afcec2467fd8857303c9f182133e9ad
rakib <rahmed@hvac.mea.com>
2025-06-20 03:57:31 -0400

    v0.2.0: Piratechs Icons

--- src/app/components/container/container.tsx
+++ src/app/components/container/container.tsx
@@ -4,22 +4,55 @@ import './container.scss';
 
 import Header from '../headers/header/header';
 import Footer from '../footers/footer/footer';
-import { createContext, useState } from 'react';
+import { debounce, devEnv } from '@/shared/scripts/constants';
+import { createContext, useEffect, useMemo, useState } from 'react';
 
 export const State = createContext({});
 
 export default function Container({ children, className = `containerComponent` }: any) {
+    let [width, setWidth] = useState<any>(1920);
+    let [loaded, setLoaded] = useState<any>(false);
+    let [isDevEnv, setDevEnv] = useState<any>(devEnv);
     let [menuExpanded, setMenuExpanded] = useState<any>(false);
+
+    useEffect(() => {
+        const onResize = () => {
+            const newWidth = window?.innerWidth;
+            setWidth((prevWidth?: number) => (prevWidth !== newWidth ? newWidth : prevWidth));
+        }
+
+        const debouncedResize = debounce(onResize, 5);
+
+        onResize();
+        setLoaded(true);
+
+        window?.addEventListener(`resize`, debouncedResize);
+        return () => window?.removeEventListener(`resize`, debouncedResize);
+    }, []);
+
+    const state = useMemo(() => ({
+        width, setWidth,
+        loaded, setLoaded,
+        isDevEnv, setDevEnv,
+        menuExpanded, setMenuExpanded,
+    }), [width, loaded, isDevEnv, menuExpanded]);
+
     return (
-        <State.Provider value={{
-            menuExpanded, setMenuExpanded, 
-        }}>
+        <State.Provider value={state}>
             <body className={`${className} pageContainer`}>
+
                 <Header />
+
                 <main className={`container`}>
+                
+                    {/* {loaded && isDevEnv ? width : ``} */}
+
                     {children}
+
                 </main>
+
                 <Footer />
+
             </body>
         </State.Provider>
     )
--- src/shared/scripts/constants.ts
+++ src/shared/scripts/constants.ts
@@ -15,3 +15,49 @@ export const constants = {
         },
     },
 }
+
+export const debounce = (func: (...args: any[]) => void, wait: number) => {
+    let timeout: any;
+    return (...args: any[]) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func(...args), wait);
+    };
+}
+
+export const urlHostMatches = (envs: string[]) => {
+    let hostMatched = false;
+    let windowEnabled = typeof window !== `undefined`;
+    if (windowEnabled) {
+        hostMatched = envs.some(env => window?.location?.host?.includes(env));
+    }
+    return hostMatched;
+}
+
+export const development = process?.env?.NODE_ENV == `development`;
+export const devEnv = urlHostMatches([`local`, `:3000`]) || development;
+
+export const dev = (item?: any, source?: any) => {
+    if (item) {
+        console.log(`Dev Log`, item);
+    } else if (item && source) {
+        console.log(`Dev Log`, item, `From`, source);
+    }
+    return devEnv;
+}
+
+// Check if Item is Not Null, Not Undefined, Not a Zero Value, etc.
+export const isValid = (item: any) => {
+    if (typeof item == `string`) {
+        let isInvalidString = !item || item == `` || item.trim() == `` || item == undefined || item == null;
+        return !isInvalidString;
+    } else if (typeof item == `number`) {
+        let isInvalidNumber = isNaN(item) || item == undefined || item == null;
+        return !isInvalidNumber;
+    } else if (typeof item == `object` && item != undefined && item != null) {
+        let isInvalidObject = Object.keys(item).length == 0 || item == undefined || item == null;
+        return !isInvalidObject;
+    } else {
+        let isUndefined = item == undefined || item == null;
+        return !isUndefined;
+    }
+}
