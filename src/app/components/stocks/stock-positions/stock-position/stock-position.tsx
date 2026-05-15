f023139d102dcd5e5b7af8b7e8c809128f5bcdc7
Rakib Ahmed <RAhmed@hvac.mea.com>
2026-05-15 09:52:43 -0400

    v0.4.6.7: Stock Realtime Updates Tracked

--- src/app/components/stocks/stock-positions/stock-position/stock-position.tsx
+++ src/app/components/stocks/stock-positions/stock-position/stock-position.tsx
@@ -83,7 +83,7 @@ export default function StockPostion({
                         </strong>
                         <div></div>
                         <strong className={`stockStat stockStatLastUpdated`}>
-                            <i><span className={`main`}>Last</span> <>{<IconText showIcon={false} text={getLastUpdated(String(stock?.tracked_last_updated))} />}</></i>
+                            <i><span className={`main`}>Last</span> <>{<IconText showIcon={false} text={getLastUpdated(String(stock?.lastUpdate))} />}</></i>
                         </strong>
                     </>}
                 </div>
